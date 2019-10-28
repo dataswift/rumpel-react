90904e97df49631cdc9513a217726067d9e31ca1
leymytel <leymytel@gmail.com>
2019-10-28 14:32:17 +0000

    Init testing envrironment and node-sass

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HatClaim from "../hat-claim/HatClaim";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <HatClaim />
+        Rumpel
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/hat-claim/HatClaim/HatClaim.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HatClaim.scss';
+
+const HatClaim: React.FC = () => {
+    return (
+        <div className="hat-claim">
+            Rumpel
+        </div>
+    );
+};
+
+export default HatClaim;
