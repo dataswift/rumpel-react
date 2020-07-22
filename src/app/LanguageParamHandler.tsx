8adf59d7ae98e1930fb0978f7095d099f22abad3
Eleftherios Myteletsis <eleftherios.myteletsis@dataswift.io>
2020-07-22 12:41:08 +0100

    DSE-363 Add language support for the HMI screen

--- src/app/LanguageParamHandler.tsx
+++ src/app/LanguageParamHandler.tsx
@@ -2,16 +2,26 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setLanguage } from '../features/language/languageSlice';
 import { fetchMessages } from '../features/messages/messagesSlice';
+import * as queryString from "query-string";
+import {config} from "../app.config";
 
 type Props = {
   children: React.ReactNode;
 };
 
+type Query = {
+  lang?: string;
+}
+
 export const LanguageParamHandler: React.FC<Props> = props => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setLanguage('en'));
+    const { lang } = queryString.parse(window.location.search) as Query;
+    const isValid = config.acceptedLanguages.indexOf(lang ? lang : config.defaultLanguage) !== -1;
+    const language = isValid && lang ? lang : config.defaultLanguage;
+
+    dispatch(setLanguage(language));
     dispatch(fetchMessages());
   }, [dispatch]);
 
