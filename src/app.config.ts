4f3700b5900a63602ae7927fa4c39bd43c456bcb
Eleftherios Myteletsis <eleftherios.myteletsis@dataswift.io>
2020-09-24 13:05:16 +0100

    Bump version from 4.0.10 to 4.1.0, upgrade hmi library

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 import { environment } from './environment';
 
 export const config = {
-  version: '4.0.10.0',
+  version: '4.1.0',
   name: environment.appName,
   tokenApp: environment.tokenName,
   tokenExpiryTime: 3,
