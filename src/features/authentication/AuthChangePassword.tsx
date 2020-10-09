4134c1204502f8aefa2125172a399c5ac9d4a776
Daniel Deak <daniel.deak@dataswift.io>
2020-10-09 12:07:48 +0100

    Run yarn upgrade to bring dependancies to latest versions
    Changed: yarn upgrade was run to bring all package versions within
    both package.json and yarn.lock to their latest version.
    Fixed: Due to the above, fixed a couple of typescript errors related
    to useParams.

--- src/features/authentication/AuthChangePassword.tsx
+++ src/features/authentication/AuthChangePassword.tsx
@@ -1,29 +1,29 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './AuthLogin.scss';
-import { resetPassword } from "../../api/hatAPI";
-import { AgreementsModal, AuthApplicationLogo, Input, IssuedBy } from "hmi";
-import { PasswordStrengthIndicator } from "../../components/PasswordStrengthMeter/PasswordStrengthIndicator";
-import { loadDynamicZxcvbn } from "../../utils/load-dynamic-zxcvbn";
-import { useHistory, useParams } from "react-router";
+import { resetPassword } from '../../api/hatAPI';
+import { AgreementsModal, AuthApplicationLogo, Input, IssuedBy } from 'hmi';
+import { PasswordStrengthIndicator } from '../../components/PasswordStrengthMeter/PasswordStrengthIndicator';
+import { loadDynamicZxcvbn } from '../../utils/load-dynamic-zxcvbn';
+import { useHistory, useParams } from 'react-router';
 
-import * as queryString from "query-string";
-import { useDispatch, useSelector } from "react-redux";
+import * as queryString from 'query-string';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   getApplicationHmi,
   selectApplicationHmi,
   selectApplicationHmiState,
-  setAppsHmiState
-} from "../applications/applicationsSlice";
-import FormatMessage from "../messages/FormatMessage";
-import { selectLanguage } from "../language/languageSlice";
-import { selectMessages } from "../messages/messagesSlice";
+  setAppsHmiState,
+} from '../applications/applicationsSlice';
+import FormatMessage from '../messages/FormatMessage';
+import { selectLanguage } from '../language/languageSlice';
+import { selectMessages } from '../messages/messagesSlice';
 
 type Query = {
   email?: string;
   application_id?: string;
   lang?: string;
   redirect_uri?: string;
-}
+};
 
 const debounce = require('lodash.debounce');
 
@@ -41,14 +41,19 @@ const AuthChangePassword: React.FC = () => {
   const [email, setEmail] = useState('');
   const [score, setScore] = useState(0);
   const [errorMessage, setErrorMessage] = useState('');
-  const [errorSuggestion, setErrorSuggestion] = useState("");
+  const [errorSuggestion, setErrorSuggestion] = useState('');
   const dispatch = useDispatch();
   const [openPopup, setOpenPopup] = useState(false);
   const [passwordMatch, setPasswordMatch] = useState<boolean | undefined>(undefined);
   const [successfulResponse, setSuccessfulResponse] = useState<Date | null>(null);
-  let { resetToken } = useParams();
-  const passwordMatchDebounce = useRef(debounce((password: string, passwordConfirm: string, score: number) =>
-    validatePasswordMatch(password, passwordConfirm, score), 400)).current;
+  let { resetToken } = useParams<{ resetToken: string }>();
+  const passwordMatchDebounce = useRef(
+    debounce(
+      (password: string, passwordConfirm: string, score: number) =>
+        validatePasswordMatch(password, passwordConfirm, score),
+      400,
+    ),
+  ).current;
 
   const validatePasswordMatch = (password: string, passwordConfirm: string, score: number) => {
     if (score > 2) {
@@ -91,9 +96,9 @@ const AuthChangePassword: React.FC = () => {
 
   const onPasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    if (name === "password-1") {
+    if (name === 'password-1') {
       validatePassword(value);
-    } else if (name === "password-2") {
+    } else if (name === 'password-2') {
       setPasswordConfirm(value);
     }
   };
@@ -109,18 +114,18 @@ const AuthChangePassword: React.FC = () => {
       const { application_id, redirect_uri, email, lang } = queryString.parse(window.location.search) as Query;
 
       if (redirect_uri && application_id) {
-        const path = `/auth/oauth?application_id=${ application_id }`+
-            `&redirect_uri=${ redirect_uri }&email=${ email }&lang=${ lang }`;
+        // eslint-disable-next-line max-len
+        const path = `/auth/oauth?application_id=${application_id}&redirect_uri=${redirect_uri}&email=${email}&lang=${lang}`;
         history.replace(path);
       } else {
-        history.replace("/auth/login");
+        history.replace('/auth/login');
       }
     }
   };
 
   useEffect(() => {
     const { email, application_id } = queryString.parse(window.location.search) as Query;
-    setEmail(email || "");
+    setEmail(email || '');
 
     loadDynamicZxcvbn(() => {
       // zxcvbn ready
@@ -151,73 +156,72 @@ const AuthChangePassword: React.FC = () => {
 
         <h2 className={'ds-hmi-email auth-login-email-title'}>{email}</h2>
 
-        {successfulResponse &&
-            <>
-              <h2 className={'auth-login-title'}>
-                <FormatMessage id={'ds.auth.changePassword.success.title'} />
-              </h2>
+        {successfulResponse && (
+          <>
+            <h2 className={'auth-login-title'}>
+              <FormatMessage id={'ds.auth.changePassword.success.title'} />
+            </h2>
 
-              <button className={'auth-login-btn ds-hmi-btn'}
-                onClick={() => login()}
-              >
-                <FormatMessage id={'ds.auth.loginBtn'} />
-              </button>
-            </>
-        }
+            <button className={'auth-login-btn ds-hmi-btn'} onClick={() => login()}>
+              <FormatMessage id={'ds.auth.loginBtn'} />
+            </button>
+          </>
+        )}
 
-        {!successfulResponse &&
-        <>
-          <h2 className={'auth-login-title'}>
-            <FormatMessage id={'ds.auth.changePassword.title'} />
-          </h2>
-          <Input type={'password'}
-            autoComplete={'new-password'}
-            name={'password-1'}
-            id={'password-1'}
-            placeholder={messages['ds.auth.input.password']}
-            value={password}
-            hasError={!!errorMessage}
-            passwordMatch={passwordMatch}
-            onChange={e => onPasswordChange(e)} />
+        {!successfulResponse && (
+          <>
+            <h2 className={'auth-login-title'}>
+              <FormatMessage id={'ds.auth.changePassword.title'} />
+            </h2>
+            <Input
+              type={'password'}
+              autoComplete={'new-password'}
+              name={'password-1'}
+              id={'password-1'}
+              placeholder={messages['ds.auth.input.password']}
+              value={password}
+              hasError={!!errorMessage}
+              passwordMatch={passwordMatch}
+              onChange={(e) => onPasswordChange(e)}
+            />
 
-          {password.length > 0 &&
-          <PasswordStrengthIndicator strong={score > 2} passwordMatch={passwordMatch}/>
-          }
+            {password.length > 0 && <PasswordStrengthIndicator strong={score > 2} passwordMatch={passwordMatch} />}
 
-          {score >= 3 &&
-          <Input type={'password'}
-            placeholder={messages['ds.auth.input.confirmPassword']}
-            autoComplete={'new-password'}
-            name={'password-2'}
-            id={'password-2'}
-            hidden={score < 3}
-            value={passwordConfirm}
-            hasError={!!errorMessage}
-            errorMessage={errorMessage}
-            errorSuggestion={errorSuggestion}
-            passwordMatch={passwordMatch}
-            onChange={e => onPasswordChange(e)}
-          />
-          }
+            {score >= 3 && (
+              <Input
+                type={'password'}
+                placeholder={messages['ds.auth.input.confirmPassword']}
+                autoComplete={'new-password'}
+                name={'password-2'}
+                id={'password-2'}
+                hidden={score < 3}
+                value={passwordConfirm}
+                hasError={!!errorMessage}
+                errorMessage={errorMessage}
+                errorSuggestion={errorSuggestion}
+                passwordMatch={passwordMatch}
+                onChange={(e) => onPasswordChange(e)}
+              />
+            )}
 
-          {passwordMatch &&
-          <div className={'auth-login-text'} onClick={() => setOpenPopup(!openPopup)}>
-            <FormatMessage id={'ds.auth.changePassword.byProceeding'} asHtml={true} />
-          </div>
-          }
+            {passwordMatch && (
+              <div className={'auth-login-text'} onClick={() => setOpenPopup(!openPopup)}>
+                <FormatMessage id={'ds.auth.changePassword.byProceeding'} asHtml={true} />
+              </div>
+            )}
 
-          <button className={'auth-login-btn ds-hmi-btn'}
-            disabled={score < 3 || !passwordMatch}
-            onClick={() => validateAndReset()}
-          >
-            <FormatMessage id={'ds.auth.nextBtn'} />
-          </button>
-        </>
-        }
+            <button
+              className={'auth-login-btn ds-hmi-btn'}
+              disabled={score < 3 || !passwordMatch}
+              onClick={() => validateAndReset()}
+            >
+              <FormatMessage id={'ds.auth.nextBtn'} />
+            </button>
+          </>
+        )}
 
-
-        <IssuedBy language={language}/>
-        <AgreementsModal language={language} open={openPopup} onClose={() => setOpenPopup(!openPopup)}/>
+        <IssuedBy language={language} />
+        <AgreementsModal language={language} open={openPopup} onClose={() => setOpenPopup(!openPopup)} />
       </div>
     </div>
   );
