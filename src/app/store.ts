f60741590314f6bf65cc0b04a811c332dee335bc
Daniel Deak <daniel.deak@dataswift.io>
2020-10-30 11:50:04 +0000

    Add Data Debit page.
    Added:
    - New landing page for Data Debits, which displays the header
    and list of data debit cards.
    - New state in Redux to hold the Data Debit items.
    - Data Debit test data.
    
    Changed:
    - Updated styles on Card objects to more accurately match the existing
    designs.

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,14 +3,15 @@ import applicationsSlice from '../features/applications/applicationsSlice';
 import authenticationSlice from '../features/authentication/authenticationSlice';
 import languageSlice from '../features/language/languageSlice';
 import messagesSlice from '../features/messages/messagesSlice';
-import hatLoginSlice from "../features/hat-login/hatLoginSlice";
-import hmiSlice from "../features/hmi/hmiSlice";
-import hatClaimCombinedReducer from "../features/hat-claim/redux/reducers/hatClaimCombinedReducer";
-import toolsSlice from "../features/tools/toolsSlice";
-import universalDataViewerSlice from "../features/universal-data-viewer/universalDataViewerSlice";
-import publicProfileSlice from "../features/public-profile/publicProfileSlice";
-import systemStatusSlice from "../features/system-status/systemStatusSlice";
-import profileSlice from "../features/profile/profileSlice";
+import hatLoginSlice from '../features/hat-login/hatLoginSlice';
+import hmiSlice from '../features/hmi/hmiSlice';
+import hatClaimCombinedReducer from '../features/hat-claim/redux/reducers/hatClaimCombinedReducer';
+import toolsSlice from '../features/tools/toolsSlice';
+import universalDataViewerSlice from '../features/universal-data-viewer/universalDataViewerSlice';
+import publicProfileSlice from '../features/public-profile/publicProfileSlice';
+import systemStatusSlice from '../features/system-status/systemStatusSlice';
+import profileSlice from '../features/profile/profileSlice';
+import dataDebitsSlice from '../features/data-debit/dataDebitSlice';
 
 export const store = configureStore({
   reducer: {
@@ -26,6 +27,7 @@ export const store = configureStore({
     systemStatus: systemStatusSlice,
     profile: profileSlice,
     hatClaim: hatClaimCombinedReducer,
+    dataDebits: dataDebitsSlice,
   },
 });
 
