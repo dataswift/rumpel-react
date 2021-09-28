d700f97f7b72423f08b20d45d8b7b34428f9f55e
Eleftherios Myteletsis <eleftherios.myteletsis@dataswift.io>
2021-09-28 10:49:58 +0100

    DAS-808 Added Registration page

--- src/types/Hatters.ts
+++ src/types/Hatters.ts
@@ -3,3 +3,18 @@ export interface PdaLookupResponse {
   hatName: string;
   verified: boolean;
 }
+
+export interface PdaSignup {
+  email: string;
+  applicationId: string;
+  platform?: string;
+  tags?: string[];
+  redirectUri: string;
+  nextStep?: string;
+  newsletterOptin?: boolean;
+}
+
+export interface RegistrationRedirectError {
+  error: string;
+  reason: string;
+}
